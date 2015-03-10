ed6da3cb8da4783081f95569990ade6a18e205e6
Ashley Davis <ashley.davis@realseriousgames.com>
2015-03-10 13:56:35 +1000

    Added a dependency injected MonoBehaviour to the example.

---
+++ Assets/ExampleDependencyInjectedMonoBehavior.cs
@@ -0,0 +1,21 @@
+﻿using RSG;
+using RSG.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class ExampleDependencyInjectedMonoBehavior : MonoBehaviour
+{
+    [Dependency]
+    public ILogger Logger { get; set; }
+
+    void Start()
+    {
+        // Assume the App singleton has already been booted!
+        App.Instance.Factory.ResolveDependencies(this);
+
+        Logger.LogInfo("I'm a dependency injected MonoBehaviour!");
+    }
+}
